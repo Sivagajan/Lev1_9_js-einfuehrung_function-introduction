b3c9a8778b6ac4b16bd1387c24f44dba8f1ee4f6
Sivagajan <sivagajan@gmail.com>
2022-07-28 14:43:29 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+console.log('works')
+
+
+let introduction = (firstName, lastName, birthPlace, age, domicile) => {
+    
+    console.log('Mein Name ist ' + firstName + ' ' + lastName)
+    console.log('Ich bin in ' + birthPlace +' geboren')
+    console.log('Ich bin ' + age + ' Jahre alt ')
+    console.log('ich wohne in ' + domicile)
+}
+
+introduction('Sivagajan', 'Karunanathan', 'Heppenheim', 33, 'Mönchengladbach')
